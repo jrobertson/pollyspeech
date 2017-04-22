97fed79212b7599a05e500b75d549b8a78905899
Your Name <you@example.com>
2017-04-22 11:46:03 +0100

    1st commit

---
+++ lib/pollyspeech.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+# file: pollyspeech.rb
+
+
+require 'aws-sdk'
+require 'digest/md5'
+
+
+class PollySpeech
+
+
+  def initialize(access_key: '', secret_key: '', region: 'us-east-1', 
+                  voice_id: 'Emma', cache_filepath: 'cache')
+
+    @polly = Aws::Polly::Client.new(region: region, 
+      credentials: Aws::Credentials.new(access_key, secret_key))
+    @voice_id = voice_id
+    @cache_filepath = cache_filepath
+
+  end
+
+  def tts(text='', audiofile_out='/tmp/polly.ogg')
+
+    ext = File.extname(audiofile_out)
+    out_format = {'.ogg' => 'ogg_vorbis', '.mp3' => 'mp3'}[ext]
+
+    FileUtils.mkdir_p @cache_filepath
+    
+    h = Digest::MD5.new << text
+    filename = File.join(@cache_filepath, h.to_s + ext)
+    
+    
+    # attempts to find the audio file from a local cache instead of 
+    # making a relatively expensive request through the web API
+    
+    if not File.exists? filename then
+            
+      @polly.synthesize_speech(response_target: filename, text: text, 
+        output_format: out_format,  voice_id: @voice_id)
+
+    end    
+    
+    FileUtils.cp filename, audiofile_out    
+
+  end
+
+end
---
+++ pollyspeech.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name = 'pollyspeech'
+  s.version = '0.1.0'
+  s.summary = 'A wrapper  of the aws-sdk-polly gem which caches audio files locally. ' + 
+      'Defaults to a British voice and OGG format.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/pollyspeech.rb']
+  s.add_runtime_dependency('aws-sdk', '~> 2.9', '>=2.9.11')
+  s.signing_key = '../privatekeys/pollyspeech.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/pollyspeech'
+end
